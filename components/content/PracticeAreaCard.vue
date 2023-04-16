309802425acccd0cb45b654d76171b32b710fa52
Joel Poulin <joel@jota.one>
2023-04-16 12:10:06 +0200

    feat: practice areas dynamic list

---
+++ components/content/PracticeAreaCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <NuxtLink class="card" :href="pa.link">
+    <h3 class="title">{{ pa.title }}</h3>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+export interface PracticeArea {
+  title: string
+  link: string
+}
+
+type Props = {
+  pa: PracticeArea
+}
+
+defineProps<Props>()
+</script>
+
+<style lang="postcss" scoped>
+@import '/assets/styles/_mediaquery.pcss';
+
+.card {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  max-width: 15rem;
+  border: 2px solid #adadad;
+
+  padding: 5px;
+  background-color: #e7eeef;
+  overflow: hidden;
+  align-items: center;
+
+  @media (--desktop) {
+    width: 12rem;
+  }
+
+  &:hover {
+  }
+
+  .title {
+    width: 100%;
+    text-align: center;
+    font-size: 21px;
+    padding: 0 15%;
+  }
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: rgb(var(--color-white));
+  background: rgb(var(--color-primary), 0.7);
+  opacity: 0;
+  transition: opacity 0.1s linear;
+}
+</style>
--- components/content/PracticeAreas.vue
+++ components/content/PracticeAreas.vue
@@ -1,12 +1,36 @@
 <template>
-  <pre>{{ data }}</pre>
+  <div class="cards">
+    <PracticeAreaCard v-for="pa in practiceAreas" :key="pa.link" :pa="pa" />
+  </div>
 </template>
 
 <script setup lang="ts">
-const { data } = await useAsyncData('pa', () => queryContent('/en/practice-areas/').only(['title', '_path']).find())
+const { data } = await useAsyncData('pa', () =>
+  queryContent('/en/practice-areas/').only(['title', '_path']).find(),
+)
 
+const practiceAreas = computed(() =>
+  (data.value || []).map(item => ({
+    title: item.title,
+    link: item._path,
+  })),
+)
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
+@import '/assets/styles/_mediaquery.pcss';
 
+.cards {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  margin-bottom: 2rem;
+
+  @media (--desktop) {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+}
 </style>
